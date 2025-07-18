14ee508b3d3a916ced398e4ac720d850883e6e3e
saiteja6111 <saitejain8055@gmail.com>
2025-07-18 17:35:36 +0530

    code changes in main

--- new_oops.py
+++ new_oops.py
@@ -7,6 +7,38 @@ class Student:
         self.grade = grade
     def get_grade(self):
         return self.grade
+class Course:
+    def __init__(self,name,max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+        self.is_Active = False
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+    
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
+
+s1 = Student("saiteja",20,95)
+s2 = Student("ram",20,75)
+s3 = Student("tony",21,65)
+
+course =Course("english",2)
+
+course.add_student(s1)
+course.add_student(s2)
+
+print(course.get_average_grade())
+
+
+
+
     
     
 
--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -3,6 +3,32 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-coffemake1 = CoffeeMaker()
 
-coffemake1.report()
+coffemake1 = CoffeeMaker()
+menu1 = Menu()
+menuitem1 = MenuItem('latte',200,150,24,2.50)
+menuitem2 = MenuItem('espresso',50,0,18,1.50)
+menuitem3 = MenuItem('cappuccino',250,100,24,3.00)
+Money1 = MoneyMachine()
+
+while True:
+    user_choice = input(f"Enter your choice {menu1.get_items()} : ")
+    if user_choice.lower() == menuitem1.name:
+        Money1.make_payment(2.50)
+        coffemake1.is_resource_sufficient(menuitem1)
+        coffemake1.make_coffee(menuitem1)
+    elif user_choice.lower() == menuitem2.name:
+        Money1.make_payment(1.50)
+        coffemake1.is_resource_sufficient(menuitem2)
+        coffemake1.make_coffee(menuitem2)
+    elif user_choice.lower() == menuitem3.name:
+        Money1.make_payment(3.00)
+        coffemake1.is_resource_sufficient(menuitem3)
+        coffemake1.make_coffee(menuitem3)
+    elif user_choice.lower() == 'report':
+        coffemake1.report()
+
+
+
+
+
